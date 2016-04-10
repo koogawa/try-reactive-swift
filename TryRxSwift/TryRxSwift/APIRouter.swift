685b5eb3b55f4c63abcc951e31ee637663a0c1d0
naoto0822 <n.h.in.m.h@gmail.com>
2016-04-11 00:21:31 +0900

    touch api, constants, router class

---
+++ TryRxSwift/TryRxSwift/APIRouter.swift
@@ -0,0 +1,13 @@
+//
+//  APIRouter.swift
+//  TryRxSwift
+//
+//  Created by naoto yamaguchi on 2016/04/11.
+//  Copyright © 2016年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class APIRouter: NSObject {
+
+}
