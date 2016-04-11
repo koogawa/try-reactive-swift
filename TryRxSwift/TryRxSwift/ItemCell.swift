323d56a481a8e06e66c23d6970d85045d782d87b
naoto0822 <n.h.in.m.h@gmail.com>
2016-04-11 23:21:55 +0900

    modify ViewModel

---
+++ TryRxSwift/TryRxSwift/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  TryRxSwift
+//
+//  Created by naoto yamaguchi on 2016/04/11.
+//  Copyright © 2016年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
