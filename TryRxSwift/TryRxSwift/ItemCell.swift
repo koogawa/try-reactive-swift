6af87abe6e9a4d6b19947b232c06b0ab3dde1d16
naoto0822 <n.h.in.m.h@gmail.com>
2016-04-11 23:58:56 +0900

    layout item cell

--- TryRxSwift/TryRxSwift/ItemCell.swift
+++ TryRxSwift/TryRxSwift/ItemCell.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import WebImage
 
 class ItemCell: UITableViewCell {
-
+    
+    // MARK: - Property
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userIDLabel: UILabel!
+    @IBOutlet weak var itemTitleLabel: UILabel!
+    let cellModel = ItemCellModel()
+    let disposeBag = DisposeBag()
+    
+    // MARK: - LifeCycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.bind()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -21,4 +35,34 @@ class ItemCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // MARK: - Private
+    
+    private func bind() {
+        
+        self.cellModel.imageURL
+            .asDriver()
+            .filter { (text) -> Bool in
+                text.characters.count > 0
+            }
+            .driveNext { (text) -> Void in
+                if let url = NSURL(string: text) {
+                    self.userImageView.sd_setImageWithURL(
+                        url,
+                        placeholderImage: nil,
+                        options: .RetryFailed
+                    )
+                }
+            }
+            .addDisposableTo(self.disposeBag)
+        
+        self.cellModel.userID
+            .asDriver()
+            .drive(self.userIDLabel.rx_text)
+            .addDisposableTo(self.disposeBag)
+        
+        self.cellModel.title
+            .asDriver()
+            .drive(self.itemTitleLabel.rx_text)
+            .addDisposableTo(self.disposeBag)
+    }
 }
