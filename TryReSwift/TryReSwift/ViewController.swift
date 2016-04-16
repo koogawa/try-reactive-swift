77f4bced71f0d181bf02af6847376894fe616abc
naoto0822 <n.h.in.m.h@gmail.com>
2016-04-16 14:14:20 +0900

    add TryReSwift proj

---
+++ TryReSwift/TryReSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryReSwift
+//
+//  Created by naoto yamaguchi on 2016/04/16.
+//  Copyright © 2016年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
